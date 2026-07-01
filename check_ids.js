885e397bd93bbd010110261e8dd3d74c75af0fc4
pcdosilva01-spec <pcdosilva01@gmail.com>
2026-07-01 14:32:59 -0300

    update

--- check_ids.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const js = fs.readFileSync('script.js', 'utf8');
-const html = fs.readFileSync('index.html', 'utf8');
-const matches = js.match(/el\("([^"]+)"\)/g) || [];
-const ids = [...new Set(matches.map(m => m.replace(/el\("|"\)/g, '')))];
-const missing = ids.filter(id => !html.includes('id="' + id + '"'));
-console.log('Total IDs no JS:', ids.length);
-console.log('Ausentes no HTML:', missing.length ? missing.join(', ') : 'nenhum');
